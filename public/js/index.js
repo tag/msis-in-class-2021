785b4cb3242a72a4bee3a7636590689515748268
jfolz <jackfolz98@gmail.com>
2021-09-13 13:53:16 -0400

    adding data to index.html and index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,34 @@
 const Offer = {
     data() {
       return {
-        foo: 0,
-        msg: "D&S is my favorite",
-        list: ["red", "green", "blue"]
+        "offer":[
+            {
+                "id": 1,
+                "name": "Joe Doe",
+                "salary": 72000,
+                "bonus": 5000,
+                "company": "KMPG",
+                "offerDate": "2021-09-13"
+            },
+    
+            {
+                "id": 2,
+                "name": "Jack French",
+                "salary": 90000,
+                "bonus": 10000,
+                "company": "PWC",
+                "offerDate": "2021-09-01"
+            },
+    
+            {
+                "id": 3,
+                "name": "Shane Efram",
+                "salary": 85000,
+                "bonus": 9000,
+                "company": "EY",
+                "offerDate": "2021-09-08"
+            }
+        ]
       }
     }
   }
