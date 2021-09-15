38471b188ede0441adf469bf4c7a2958003aa76d
Tom G <tomg@byu.net>
2021-09-15 09:41:26 -0400

    fetch! in class 15 Sep

--- public/js/index.js
+++ public/js/index.js
@@ -1,26 +1,44 @@
-const Offer = {
+
+const SomeApp = {
     data() {
       return {
-        "offers": [
-                {
-                    "id": 1,
-                    "name": "Jordan Doe",
-                    "salary": 120000,
-                    "bonus": 9000,
-                    "company": "EY",
-                    "offerDate": "2021-10-12"
-                },
-                {
-                    "id": 2,
-                    "name": "Jesse Doe",
-                    "salary": 90000,
-                    "bonus": 2000,
-                    "company": "IU",
-                    "offerDate": "2021-12-10"
-                }
-            ]
-        }
+        result: {},
+        list: [5,6,7,8],
+        message: "Waiting ..."
+      }
+    },
+    created() {
+
+        //Method 1:
+        fetch('https://randomuser.me/api/')
+        .then(response => response.json())
+        .then((json) => {
+            console.log("Got json back:", json);
+            this.result = json.results[0];
+            console.log("C");
+        })
+        .catch( (error) => {
+            console.error(error);
+        });
+
+        console.log("B");
+        /*
+            .then(response => response.json())
+        Is the same as
+            .then(function(response) {return response.json()})
+        */
+
+
+        //Method 2:
+        // const response = await fetch("https://randomuser.me/api/");
+        // const responseJson = await response.json();
+
+        // console.log("Two:", responseJson);
+        // this.message = responseJson.results[0].name;
+        // this.result = responseJson.results[0];
+        
     }
+
   }
   
-  Vue.createApp(Offer).mount('#offerApp')
+  Vue.createApp(SomeApp).mount('#someApp');
