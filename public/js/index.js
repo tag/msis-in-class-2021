0ab976ab0204a2577e27c8904169f79833548021
Tom G <tomg@byu.net>
2021-09-20 09:53:21 -0400

    datajs, methods
    datajs, methods

--- public/js/index.js
+++ public/js/index.js
@@ -23,19 +23,30 @@ const Offer = {
             ]
         }
     },
+    computed: {
+        prettyBirthday() {
+            return dayjs(this.person.dob.date)
+            .format('D MMM YYYY')
+        }
+    },
+    methods: {
+        fetchUserData() {
+            console.log("A");
+            fetch('https://randomuser.me/api/')
+            .then( response => response.json() )
+            .then( (responseJson) => {
+                console.log(responseJson);
+                console.log("C");
+                this.person = responseJson.results[0];
+            })
+            .catch( (err) => {
+                console.error(err);
+            })
+            console.log("B");
+        }
+    },
     created() {
-        console.log("A");
-        fetch('https://randomuser.me/api/')
-        .then( response => response.json() )
-        .then( (responseJson) => {
-            console.log(responseJson);
-            console.log("C");
-            this.person = responseJson.results[0];
-        })
-        .catch( (err) => {
-            console.error(err);
-        })
-        console.log("B");
+        this.fetchUserData();
     } //end created
 } // end Offer config
   
