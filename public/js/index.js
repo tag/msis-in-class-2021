fac31aee4d35e708c7fdc5a5471da836d8744396
Tom G <tomg@byu.net>
2021-09-20 15:17:17 -0400

    computed; methods; datejs

--- public/js/index.js
+++ public/js/index.js
@@ -22,21 +22,30 @@ const Offer = {
             ]
         }
     },
+    computed: {
+        prettyBirthday() {
+            return dayjs(this.person.dob.date)
+            .format('D MMM YYYY');
+        }
+    },
+    methods: {
+        fetchUserData() {
+            fetch('https://randomuser.me/api/')
+            .then(response => response.json())
+            .then((parsedJson) => {
+                console.log(parsedJson);
+                this.person = parsedJson.results[0]
+                console.log("C");
+            })
+            .catch( err => {
+                console.error(err)
+            })
+
+            console.log("B");
+        }
+    },
     created() {
-        console.log("A");
-
-        fetch('https://randomuser.me/api/')
-        .then(response => response.json())
-        .then((parsedJson) => {
-            console.log(parsedJson);
-            this.person = parsedJson.results[0]
-            console.log("C");
-        })
-        .catch( err => {
-            console.error(err)
-        })
-
-        console.log("B");
+        this.fetchUserData();
     }
   }
   
