4b692908344d2c5c3d57130f12f6546ef21d5af5
Tom G <tomg@byu.net>
2021-09-20 13:10:58 -0400

    dayjs, methods

--- public/js/index.js
+++ public/js/index.js
@@ -7,36 +7,29 @@ const SomeApp = {
         message: "Waiting ..."
       }
     },
-    created() {
-
-        //Method 1:
-        fetch('https://randomuser.me/api/')
-        .then(response => response.json())
-        .then((json) => {
-            console.log("Got json back:", json);
-            this.result = json.results[0];
-            console.log("C");
-        })
-        .catch( (error) => {
-            console.error(error);
-        });
-
-        console.log("B");
-        /*
+    computed: {
+        prettyBirthday() {
+            return dayjs(this.result.dob.date)
+            .format('D MMM YYYY')
+        }
+    },
+    methods: {
+        fetchUserData() {
+            //Method 1:
+            fetch('https://randomuser.me/api/')
             .then(response => response.json())
-        Is the same as
-            .then(function(response) {return response.json()})
-        */
-
-
-        //Method 2:
-        // const response = await fetch("https://randomuser.me/api/");
-        // const responseJson = await response.json();
-
-        // console.log("Two:", responseJson);
-        // this.message = responseJson.results[0].name;
-        // this.result = responseJson.results[0];
-        
+            .then((json) => {
+                console.log("Got json back:", json);
+                this.result = json.results[0];
+                console.log("C");
+            })
+            .catch( (error) => {
+                console.error(error);
+            });
+        }
+    },
+    created() {
+        this.fetchUserData();
     }
 
   }
