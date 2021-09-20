271074609556e799410f46a38fa0cd314a0d2e52
rohanbollareddi <rohanbolla@gmail.com>
2021-09-20 13:34:01 -0400

    1

---
+++ public/js/index.js
@@ -0,0 +1,36 @@
+const SomeApp = {
+    data() {
+      return {
+        result: {},
+        list: [5,6,7,8],
+        message: "Waiting ..."
+      }
+    },
+    computed: {
+        prettyBirthday() {
+            return dayjs(this.result.dob.date)
+            .format('D MMM YYYY')
+        }
+    },
+    methods: {
+        fetchUserData() {
+            //Method 1:
+            fetch('https://randomuser.me/api/')
+            .then(response => response.json())
+            .then((json) => {
+                console.log("Got json back:", json);
+                this.result = json.results[0];
+                console.log("C");
+            })
+            .catch( (error) => {
+                console.error(error);
+            });
+        }
+    },
+    created() {
+        this.fetchUserData();
+    }
+
+  }
+  
+  Vue.createApp(SomeApp).mount('#someApp');
