960eaf3d435629ee002b077ce2b3e84d9a36f45f
Tom G <tomg@byu.net>
2021-09-29 13:15:58 -0400

    db connection

---
+++ app/public/api/student/index.php
@@ -0,0 +1,27 @@
+<?php
+require 'class/DbConnection.php';
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$sql = 'SELECT * FROM student';
+$vars = [];
+
+// if (isset($_GET['guid'])) {
+//   // This is an example of a parameterized query
+//   $sql = 'SELECT * FROM Patient WHERE patientGuid = ?';
+//   $vars = [ $_GET['guid'] ];
+// }
+
+$stmt = $db->prepare($sql);
+$stmt->execute($vars);
+
+$students = $stmt->fetchAll();
+
+// Step 3: Convert to JSON
+$json = json_encode($students, JSON_PRETTY_PRINT);
+
+// Step 4: Output
+header('Content-Type: application/json');
+echo $json;
