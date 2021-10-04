8fd9a698654620155f23844ebc72cfa8d03052e2
Tom G <tomg@byu.net>
2021-10-04 07:01:14 -0400

    403 bug fix

--- app/public/api/student/index.php
+++ app/public/api/student/index.php
@@ -2,19 +2,11 @@
 // require 'common.php';
 require 'class/DbConnection.php';
 
-echo ":::";
-// print_R($_ENV);
-echo getenv('MYSQL_DATABASE');
-
-
 // Step 1: Get a datase connection from our helper class
 $db = DbConnection::getConnection();
 
-echo $db;
-exit;
-
 // Step 2: Create & run the query
-$sql = 'SELECT * FROM Students';
+$sql = 'SELECT * FROM student';
 $vars = [];
 
 // if (isset($_GET['guid'])) {
