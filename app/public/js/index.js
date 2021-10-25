af24f3f17c08e2c4240933c92f53199bbb056f4e
jfolz <jackfolz98@gmail.com>
2021-10-25 12:41:07 -0400

    creating update/ delete APIs

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -131,6 +131,58 @@ const SomeApp = {
         resetOfferForm() {
           this.selectedOffer = null;
           this.offerForm = {};
+        },
+
+        postEditOffer(evt) {
+          this.offerForm.studentId = this.selectedStudent.id;
+          this.offerForm.id = this.selectedOffer.id;       
+          
+          console.log("Updating!", this.offerForm);
+  
+          fetch('api/offer/update.php', {
+              method:'POST',
+              body: JSON.stringify(this.offerForm),
+              headers: {
+                "Content-Type": "application/json; charset=utf-8"
+              }
+            })
+            .then( response => response.json() )
+            .then( json => {
+              console.log("Returned from post:", json);
+              // TODO: test a result was returned!
+              this.offers = json;
+              
+              this.resetOfferForm();
+            });
+        },
+        postDeleteOffer(o) {
+          if (!confirm("Are you sure you want to delete the offer from "+o.companyName+"?")) {
+              return;
+          }
+          
+          fetch('api/offer/delete.php', {
+              method:'POST',
+              body: JSON.stringify(o),
+              headers: {
+                "Content-Type": "application/json; charset=utf-8"
+              }
+            })
+            .then( response => response.json() )
+            .then( json => {
+              console.log("Returned from post:", json);
+              // TODO: test a result was returned!
+              this.offers = json;
+              
+              this.resetOfferForm();
+            });
+        },
+        selectOffer(o) {
+          this.selectedOffer = o;
+          this.offerForm = Object.assign({}, this.selectedOffer);
+        },
+        resetOfferForm() {
+          this.selectedOffer = null;
+          this.offerForm = {};
         }
     },
     created() {
